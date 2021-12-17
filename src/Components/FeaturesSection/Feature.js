be23150c142b164e73e4e95da51da9363cb900bd
Aish0608 <60166936+Aish0608@users.noreply.github.com>
2021-12-17 12:52:12 +0530

    Update Feature.js

--- src/Components/FeaturesSection/Feature.js
+++ src/Components/FeaturesSection/Feature.js
@@ -11,12 +11,16 @@ export default function Feature() {
                 para1="Order in for yourself or for the group,"
                 para2=" with no restrictions on order value"
             />
+        
+        
             <Card
                 src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_224,h_412/4x_Live_order_zzotwy" 
                 h2="Live Order Tracking"
                 para1="Know where your order is at all times,"
                 para2=" from the restaurant to your doorstep"
             />
+        
+        
             <Card
                 src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_248,h_376/4x_-_Super_fast_delivery_awv7sn" 
                 h2="Lightning-Fast Delivery"
@@ -24,6 +28,8 @@ export default function Feature() {
                 para2="for food delivered fresh & on time"
             />
 
+        
+        
         </section>
     )
 }
